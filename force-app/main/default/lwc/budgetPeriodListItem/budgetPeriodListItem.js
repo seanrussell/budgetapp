7903cfc0f7785fff8075d6d161e907e6a2afc39c
Sean Russell <sean.russell@TRIFECTA.COM>
2019-04-19 16:22:16 -0400

    Edit and delete from list

--- force-app/main/default/lwc/budgetPeriodListItem/budgetPeriodListItem.js
+++ force-app/main/default/lwc/budgetPeriodListItem/budgetPeriodListItem.js
@@ -1,13 +1,66 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+
+import deleteBudgetPeriod from '@salesforce/apex/BudgetPeriodController.deleteBudgetPeriod';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class BudgetPeriodListItem extends LightningElement {
     @api period;
+    @track actions = [
+        { label: 'Edit', value: 'edit', iconName: 'utility:edit' },
+        { label: 'Delete', value: 'delete', iconName: 'utility:delete' },
+    ];
+
+    handleAction(event) {
+        const tileAction = event.detail.action.value;
+
+        if (tileAction === 'delete') {
+            this.deleteBudgetPeriodItem();
+        } else {
+            const selectedEvent = new CustomEvent('selected', {
+                detail: this.period.Id
+            });
+            this.dispatchEvent(selectedEvent);
+        }
+    }
+
+    deleteBudgetPeriodItem() {
+        if (this.period && this.period.Id) {
+            deleteBudgetPeriod({budgetPeriodId: this.period.Id})
+                .then(result => {
+                    this.period = {};
+                    
+                    const event = new ShowToastEvent({
+                        title: 'Delete',
+                        message: 'Budget period was deleted successfully',
+                        variant: 'success'
+                    });
+                    this.dispatchEvent(event);
+
+                    const selectedEvent = new CustomEvent('deleted', {
+                        detail: this.period.Id
+                    });
+                    this.dispatchEvent(selectedEvent);
+                })
+                .catch(error => {
+                    this.error = error;
+
+                    const event = new ShowToastEvent({
+                        title: 'Delete',
+                        message: 'Budget period unable to be deleted',
+                        variant: 'error'
+                    });
+                    this.dispatchEvent(event);
+                });
+        }
+    }
 
     handleClick(event) {
         event.preventDefault();
+        /*
         const selectedEvent = new CustomEvent('selected', {
             detail: this.period.Id
         });
         this.dispatchEvent(selectedEvent);
+        */
     }
 }
