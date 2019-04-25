5cb06ebbcb1627538def0d1b381a6ba77814f546
Sean Russell <sean.russell@TRIFECTA.COM>
2019-04-25 08:28:39 -0400

    Fix broken unit tests

--- force-app/main/default/lwc/budgetPeriodListContainer/__tests__/budgetPeriodListContainer.test.js
+++ force-app/main/default/lwc/budgetPeriodListContainer/__tests__/budgetPeriodListContainer.test.js
@@ -3,7 +3,17 @@ import { CurrentPageReference } from 'lightning/navigation';
 import BudgetPeriodListContainer from 'c/budgetPeriodListContainer';
 import { registerTestWireAdapter } from '@salesforce/lwc-jest';
 
+import { registerListener, unregisterAllListeners } from 'c/pubsub';
+
 registerTestWireAdapter(CurrentPageReference);
+
+// Mock out the event firing function to verify it was called with expected parameters.
+jest.mock('c/pubsub', () => {
+    return {
+        registerListener: jest.fn(),
+        unregisterAllListeners: jest.fn()
+    };
+});
 
 describe('c-budget-period-list-container', () => {
     afterEach(() => {
@@ -17,5 +27,12 @@ describe('c-budget-period-list-container', () => {
             is: BudgetPeriodListContainer
         });
         document.body.appendChild(element);
+
+        // Validate if pubsub got registered after connected to the DOM
+        expect(registerListener.mock.calls.length).toBe(1);
+        expect(registerListener.mock.calls[0][0]).toBe('budgetPeriodDeleted');
+
+        document.body.removeChild(element);
+         expect(unregisterAllListeners.mock.calls.length).toBe(1);
     });
 });
--- force-app/main/default/lwc/budgetPeriodSummaryContainer/__tests__/budgetPeriodSummaryContainer.test.js
+++ force-app/main/default/lwc/budgetPeriodSummaryContainer/__tests__/budgetPeriodSummaryContainer.test.js
@@ -3,13 +3,25 @@ import { CurrentPageReference } from 'lightning/navigation';
 import BudgetPeriodSummaryContainer from 'c/budgetPeriodSummaryContainer';
 import { registerTestWireAdapter } from '@salesforce/lwc-jest';
 
+import { registerListener, unregisterAllListeners } from 'c/pubsub';
+
 registerTestWireAdapter(CurrentPageReference);
+
+// Mock out the event firing function to verify it was called with expected parameters.
+jest.mock('c/pubsub', () => {
+    return {
+        registerListener: jest.fn(),
+        unregisterAllListeners: jest.fn()
+    };
+});
 
 describe('c-budget-period-summary-container', () => {
     afterEach(() => {
         while (document.body.firstChild) {
             document.body.removeChild(document.body.firstChild);
         }
+
+        jest.clearAllMocks();
     });
 
     it('initial component creation', () => {
@@ -17,5 +29,16 @@ describe('c-budget-period-summary-container', () => {
             is: BudgetPeriodSummaryContainer
         });
         document.body.appendChild(element);
+
+         // Validate if pubsub got registered after connected to the DOM
+         expect(registerListener.mock.calls.length).toBe(4);
+         expect(registerListener.mock.calls[0][0]).toBe('budgetPeriodAdded');
+         expect(registerListener.mock.calls[1][0]).toBe('budgetPeriodRemoved');
+         expect(registerListener.mock.calls[2][0]).toBe('transactionItemAdded');
+         expect(registerListener.mock.calls[3][0]).toBe('transactionItemDeleted');
+ 
+         // Validate if pubsub got unregistered after disconnected from the DOM
+         document.body.removeChild(element);
+         expect(unregisterAllListeners.mock.calls.length).toBe(1);
     });
 });
